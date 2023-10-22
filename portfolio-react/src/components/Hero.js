6d75a04cc1e91f1bfb32c27c3c459dbacc199be7
rquinterosv <rafael.quinterosv@gmail.com>
2023-10-22 15:51:12 -0700

    fixing mobile version

--- portfolio-react/src/components/Hero.js
+++ portfolio-react/src/components/Hero.js
@@ -7,7 +7,7 @@ function Hero() {
   return (
     <div class="hero-container">
       <div class="hero-section">
-        
+
         <h1>Welcome!</h1>
         <h3>I'm Rafael Quinteros</h3>
         <p>Full Stack Web Developer & Business Developer</p>
