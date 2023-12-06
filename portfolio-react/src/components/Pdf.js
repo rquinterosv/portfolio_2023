3b6ebe9ad40393e7369c715b76808d015d3b5e2f
rquinterosv <rafael.quinterosv@gmail.com>
2023-12-06 14:14:33 +1300

    Update resume Dic6

--- portfolio-react/src/components/Pdf.js
+++ portfolio-react/src/components/Pdf.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
+import { Page, Text, View, Document, StyleSheet, Image } from '@react-pdf/renderer';
+import perfilImage from '../img/perfil.jpeg'; // Importa la imagen
+import emailLogo from '../img/gmail.png';
+import phoneLogo from '../img/telephone.png';
+import linkedinLogo from '../img/linkedin.png';
+import githubLogo from '../img/github.png';
+import websiteLogo from '../img/web.png';
+import locationLogo from '../img/location.png';
 
 const styles = StyleSheet.create({
   page: {
@@ -7,60 +14,134 @@ const styles = StyleSheet.create({
     backgroundColor: '#E4E4E4',
   },
   section: {
-    margin: 10,
     padding: 10,
     flexGrow: 1,
   },
   column: {
     flexDirection: 'column',
     marginBottom: 10,
+    borderBottom: 1,
+    borderBottomColor: '#000',
+    padding: 5,
+  },
+  title: {
+    fontSize: 35,
+    marginBottom: 5,
   },
   header: {
-    fontSize: 20,
+    fontSize: 22,
     marginBottom: 5,
   },
   subHeader: {
-    fontSize: 16,
+    fontSize: 17,
     marginBottom: 5,
     fontStyle: 'italic', 
   },
   infoPresentation: {
-    fontSize: 10,
-    marginBottom: 3,
+    fontSize: 14,
+    marginBottom: 4,
     marginTop: 2,
   },
   infoItem: {
-    fontSize: 12,
-    marginBottom: 3,
+    fontSize: 14,
+    marginBottom: 5,
     fontWeight: 'bold',
+  },
+  pointItem: {
+    fontSize: 10, 
+    padding: 4,
   },
 });
 
-const Pdf = ({ datos }) => (
-  <Document>
-    <Page size="A4" style={styles.page}>
-      {/* Derecha del CV con 60% de width */}
-      <View style={[styles.section, { width: '60%' }]}>
-        <View style={styles.column}>
-          <Text style={styles.header}>{datos.name}</Text>
-          <Text style={styles.subHeader}>{datos.role}</Text>
-          <Text style={styles.infoPresentation}>{datos.presentation}</Text>
+const Pdf = ({ datos }) => {
+
+return (  
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <View style={[styles.section, { width: '60%' }]}>
+
+          <View style={styles.column}>
+            <Text style={styles.title}>Curriculum Vitae</Text>
+            <Text style={styles.header}>{datos.name}</Text>
+            <Text style={styles.subHeader}>{datos.role}</Text>
+          </View>
+
+          <View style={styles.column}>
+            <Text style={styles.subHeader}>Professional Summary</Text>
+            <Text style={styles.infoPresentation}>{datos.presentation}</Text>
+          </View>
+
+          <View style={styles.column}>
+            <Text style={styles.subHeader}>Work Experience</Text>
+            {datos.experience.map((exp, index) => (
+              <View key={index}>
+                <Text style={[styles.infoItem, { paddingTop: 3 }]}>{exp.title}</Text>
+                <View style={{ flexDirection: 'row', alignItems: 'flex-end' }}>
+                  <Text style={[styles.infoPresentation, { fontSize: 11, font: 'italic' }]}>
+                    {exp.company}
+                  </Text>
+                  <Text style={[styles.infoPresentation, { fontSize: 11, font: 'italic', marginLeft: 5 }]}>
+                    | {exp.date}
+                  </Text>
+                </View>
+                <View>
+                  {exp.description.map((point, idx) => (
+                    <View key={idx}>
+                      <Text style={styles.pointItem}>{point}</Text>
+                    </View>
+                  ))}
+                </View>
+              </View>
+            ))}
+          </View>
+
+          <View style={styles.column}>
+            <Text style={styles.subHeader}>Education</Text>
+            <Text style={styles.infoPresentation}>{datos.eductation}</Text>
+          </View>
         </View>
-      </View>
-      {/* Izquierda del CV con 40% de width */}
-      <View style={[styles.section, { width: '40%' }]}>
-        <View style={styles.column}> 
-          <Text style={styles.subHeader}>Contact Information</Text>
-          <Text style={styles.infoItem}>Email: {datos.email}</Text>
-          <Text style={styles.infoItem}>Whatsapp: {datos.whatsapp}</Text>
-          <Text style={styles.infoItem}>Phone: {datos.phone}</Text>
-          <Text style={styles.infoItem}>LinkedIn: {datos.linkedin}</Text>
-          <Text style={styles.infoItem}>Github: {datos.github}</Text>
-          <Text style={styles.infoItem}>Página web: {datos.website}</Text>
+
+        <View style={[styles.section, { width: '40%' }]}>
+          <View style={{ alignItems: 'left' }}>
+            <Image style={{ width: 150, height: 150, borderRadius: 80 }} source={perfilImage} />
+          </View>
+          <View style={styles.column}>
+            <Text style={styles.subHeader}>Contact</Text>
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 5 }}>
+              <Image style={{ width: 15, height: 15, marginRight: 5 }} source={locationLogo} />
+              <Text style={styles.infoItem}>{datos.location}</Text>
+            </View>  
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 5 }}>
+              <Image style={{ width: 15, height: 15, marginRight: 5 }} source={emailLogo} />
+              <Text style={styles.infoItem}>{datos.email}</Text>
+            </View>   
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 5 }}>
+              <Image style={{ width: 15, height: 15, marginRight: 5 }} source={phoneLogo} />
+              <Text style={styles.infoItem}>{datos.phone}</Text>
+            </View>
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 5 }}>
+              <Image style={{ width: 15, height: 15, marginRight: 5 }} source={linkedinLogo} />
+              <Text style={styles.infoItem}>{datos.linkedin}</Text>
+            </View>
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 5 }}>
+              <Image style={{ width: 15, height: 15, marginRight: 5 }} source={githubLogo} />
+              <Text style={styles.infoItem}>{datos.github}</Text>
+            </View>
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 5 }}>
+              <Image style={{ width: 15, height: 15, marginRight: 5 }} source={websiteLogo} />
+              <Text style={styles.infoItem}>{datos.website}</Text>
+            </View>                   
+          </View>
+          <View style={styles.column}>
+            <Text style={styles.subHeader}>Technical Skills</Text>
+            <Text style={styles.infoPresentation}>{datos.presentation}</Text>
+          </View>
         </View>
-      </View>
-    </Page>
-  </Document>
-);
+        {/* Más contenido del currículum */}
+      </Page>
+    </Document>
+  );
+};
 
 export default Pdf;
+
--- portfolio-react/src/pages/Home.js
+++ portfolio-react/src/pages/Home.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import { PDFViewer} from '@react-pdf/renderer';
 import '../App.css';
-import Hero from '../components/Hero';
 import Pdf from '../components/Pdf';
 
 function Home() {
@@ -13,23 +12,58 @@ function Home() {
     email: 'rafael.quinterosv@gmail.com',
     whatsapp: '+1 236 777 7984',
     phone: '029 0220 6952',
+    location: 'New Zealand',
     linkedin: 'in/rafael-quinteros/',
     github: 'github.com/rquinterosv',
     website: 'www.tusitio.com',
     experience: [
       {
-        title: 'Cargo',
-        company: 'Empresa',
-        date: 'Fecha',
-        description: 'Descripción',
+        title: 'Project Coordinator',
+        company: 'Grimme Fighters (Canada)',
+        date: 'Jul 2023 - Oct 2023',
+        description: ['Negotiated competitive electricity prices with various companies, showcasing adeptness in strategic pricing and contract negotiations.', 'Traveled extensively across the country, establishing connections and conducting negotiations, demonstrating adaptability and rapport-building skills.', 'Gained initial exposure in sales tactics and commercial engineering, laying the foundation for a career in Business Development while acquiring valuable insights into market dynamics.'],
+      },
+      {
+        title: 'Product Manager',
+        company: 'Desafío Latam (Chile)',
+        date: 'Mar 2021 - Jul 2023',
+        description: ['I drive product strategies that blend business goals, market needs, and technology, leading teams to deliver innovative solutions.', 'I manage the entire product journey, from concept to ongoing evolution, ensuring user-centric products aligned with market demands.', 'I facilitate seamless communication among technical, design, and business teams, ensuring everyone works cohesively towards a shared product vision.', 'Some of these projects include Women on IT, Desafío Latam Incubator, Codigo Futuro, and collaboration with several leading companies in Chile."'],
+      },
+      {
+        title: 'Full Stack Developer',
+        company: 'Desafío Latam (Chile)',
+        date: 'Oct 2022 - Sept 2023',
+        description: ['Web Development Introduction: HTMl/CSS, Git + Github, Bootstrap and good practice.', 'Advance CSS: Flexboxm CSS Grid and create responsive projects', 'Javascript Introduction: Proms, Arrays, Objects, Cicles, Functions, Conditions and funcional programing', 'React: Hooks, useState/Effect, Render, States, Router, useParams/Negative and good practice', 'SQL Introduction: Select, Where, Count, Javing, Joins, Creation of tables, Relation models, Postgresql', 'Backend Node/Express: NODE, EXPRESS, API REST, CORS, CRUD, JWT, Testing.'],
+      },
+      {
+        title: 'Business Developer Manager',
+        company: 'Huawei Technology Latinamerica',
+        date: 'Mar 2019 - Feb 2021',
+        description: ['Proficiently absorbed technological knowledge from a multinational corporation, enhancing skill sets.', 'Successfully collaborated with prestigious companies in Chile, fostering professional relationships and business growth.', 'Initiated my career transition into the IT realm, starting as a developer and immersing myself in the tech landscape to expand expertise.'],
+      },
+      {
+        title: 'Business Developer',
+        company: 'Ecom Energía (Chile)',
+        date: 'Jul 20218 - Mar 2019',
+        description: ['Negotiated competitive electricity prices with various companies, showcasing adeptness in strategic pricing and contract negotiations.', 'Traveled extensively across the country, establishing connections and conducting negotiations, demonstrating adaptability and rapport-building skills.', 'Gained initial exposure in sales tactics and commercial engineering, laying the foundation for a career in Business Development while acquiring valuable insights into market dynamics.'],
       },
       // Más experiencias laborales...
     ],
     education: [
       {
-        degree: 'Grado',
-        institution: 'Institución',
-        date: 'Fecha',
+        degree: 'Full Stack Developer',
+        institution: 'Canadian College of Technology and Business',
+        date: 'Jul 2023 - Dec 2023',
+      },
+      {
+        degree: 'FullStack Developer',
+        institution: 'Bootcamp Desafio Latam (Chile)',
+        date: '2021 - 2022',
+      },
+      {
+        degree: 'Business Engineer',
+        institution: 'Universidad Andres Bello (Chile)',
+        date: '2013 - 2018',
       },
       // Más información educativa...
     ],
@@ -37,18 +71,17 @@ function Home() {
   };
 
   return (
-    <>
-      <Hero />
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
-        <div style={{ textAlign: 'center' }}>
-          <h1>My Resume</h1>
-          <PDFViewer width={600} height={800}>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+      <div style={{ textAlign: 'center', width: '100%' }}>
+        <h1>My Resume</h1>
+        <div className="pdf-container">
+          <PDFViewer style={{ width: '98%', height: '70vh' }}>
             <Pdf datos={datosCV} />
           </PDFViewer>
         </div>
       </div>
-    </>
+    </div>
   );
-  }
+}
 
 export default Home;
