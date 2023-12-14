1d9f25a6a0786be78e21a3ee33ec6229dd7a6156
rquinterosv <rafael.quinterosv@gmail.com>
2023-12-15 11:30:14 +1300

    Implementando hero con Tailwind

--- portfolio-react/src/components/Hero.js
+++ portfolio-react/src/components/Hero.js
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa';
+import styles from '../css/Hero.module.css'; 
+
 
 const Hero = () => {
-  return (
-    <section className="bg-gray-900 text-white py-20">
-      <div className="container mx-auto text-center">
-        <h1 className="text-4xl font-bold mb-4">Welcome to My Portfolio</h1>
-        <p className="text-lg">Showing off my skills as a web developer!</p>
 
-        <div className="flex justify-center space-x-4 mt-8">
+  const [showText, setShowText] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowText(true);
+    }, 500); // Muestra el texto después de 500ms (ajusta este valor según lo necesites)
+  }, []);
+
+  return (
+    <div className="bg-white">
+      <div className="container mx-auto pt-12 md:pt-24 px-6 text-center">
+        <h1 className={`text-4xl md:text-5xl font-bold mb-4 ${showText ? styles['reveal-text'] : ''}`}>
+          Tailwind Starter Templates - Hero Product
+        </h1>
+        <h2 className={`text-lg md:text-xl ${showText ? styles['reveal-text'] : ''}`}>
+          This starter template contains:
+        </h2>
+        <p className="mt-8 mb-8">
+          <a href="https://github.com/tailwindtoolbox/Hero-Product/archive/master.zip" target="_blank" rel="noopener" className="inline-block bg-teal hover:bg-teal-dark text-white text-xl font-extrabold py-3 px-5 rounded shadow hover:shadow-lg mr-4">
+            Download Template
+          </a>
+          <a href="https://github.com/tailwindtoolbox/Hero-Product" target="_blank" rel="noopener" className="inline-block bg-grey-lightest hover:bg-black text-grey-darkest hover:text-white text-xl font-extrabold py-3 px-5 rounded shadow hover:shadow-lg">
+            View on GitHub
+          </a>
+        </p>
+
+        <div className="flex justify-center space-x-4">
           <a href="https://www.linkedin.com/your-profile" className="social-icon">
-            <FaLinkedin size={30} style={{ color: 'white' }} />
+            <FaLinkedin size={30} style={{ color: 'black' }} />
           </a>
           <a href="https://github.com/your-username" className="social-icon">
-            <FaGithub size={30} style={{ color: 'white' }} />
+            <FaGithub size={30} style={{ color: 'black' }} />
           </a>
           <a href="mailto:youremail@example.com" className="social-icon">
-            <FaEnvelope size={30} style={{ color: 'white' }} />
+            <FaEnvelope size={30} style={{ color: 'black' }} />
           </a>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
