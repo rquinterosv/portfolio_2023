7d7ddad204be9d989e79530b51a2ab427902290d
rquinterosv <rafael.quinterosv@gmail.com>
2023-12-18 13:41:49 +1300

    entrega 1 footer

--- portfolio-react/src/components/Footer.js
+++ portfolio-react/src/components/Footer.js
@@ -1,30 +1,31 @@
 import React from 'react';
+import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa';
 
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-8">
-      <div className="container mx-auto flex flex-col lg:flex-row items-center justify-between">
-        {/* Información de contacto */}
-        <div className="lg:w-1/3 mb-6 lg:mb-0">
-          <h3 className="text-xl font-bold mb-2">Contacto</h3>
-          <p>Email: tuemail@example.com</p>
-          <p>Teléfono: +1234567890</p>
-          {/* Otra información de contacto */}
-        </div>
-
-        {/* Redes sociales */}
-        <div className="lg:w-2/3 flex justify-center lg:justify-end space-x-4">
-          <a href="URL_FACEBOOK" target="_blank" rel="noopener noreferrer">
-            <i className="fab fa-facebook-square text-2xl"></i>
-          </a>
-          <a href="URL_TWITTER" target="_blank" rel="noopener noreferrer">
-            <i className="fab fa-twitter-square text-2xl"></i>
-          </a>
-          {/* Agrega más enlaces a tus redes sociales */}
+      <div className="container mx-auto text-center lg:text-center">
+        {/* Información personal */}
+        <div className="mx-auto lg:w-2/3">
+          <h2 className="text-2xl font-bold mb-2">Rafael Quinteros Villablanca</h2>
+          <h4 className="text-sm">
+            <FaEnvelope className="inline" /> rafael.quinterosv@gmail.com <br />
+            Teléfono: +642902206952 <br />
+            Ubicación: Auckland, New Zealand
+          </h4>
+          <div className="mt-4 flex justify-center">
+            <a href="enlace-a-linkedin" target="_blank" rel="noopener noreferrer" className="mx-4">
+              <FaLinkedin className="text-2xl" />
+            </a>
+            <a href="enlace-a-github" target="_blank" rel="noopener noreferrer" className="mx-4">
+              <FaGithub className="text-2xl" />
+            </a>
+            {/* Add more social links here */}
+          </div>
         </div>
       </div>
     </footer>
   );
-}
+};
 
 export default Footer;
